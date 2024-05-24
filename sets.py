5602badc08318fc372b50dff9edf54b444e7e76f
SimonBarilaro <162722001+SimonBarilaro@users.noreply.github.com>
2024-05-24 12:27:54 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -2,7 +2,7 @@ from sets_categories_data import (ALCOHOLS)
 
 def clean_ingredients(dish_name, dish_ingredients):
     dish_ingredients1 = set(dish_ingredients)
-    return dish_ingredients1
+    return dish_name, dish_ingredients1
 
 from sets_categories_data import ALCOHOLS
 def check_drinks(drink_name, drink_ingredients):
